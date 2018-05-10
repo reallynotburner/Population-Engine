a0f902a7bb4fdefbc77a154d8ce00e32359efa12
Joshua Brown <jbrown@ellation.com>
2018-05-09 23:33:28 -0700

    added random population generator and graphed it's output TODO: make the chart update based on the input text fields changing.

--- js/chart.js
+++ js/chart.js
@@ -1,30 +1,51 @@
-var wins = [
-    [
-        [0, 13],
-        [1, 11],
-        [2, 15],
-        [3, 15],
-        [4, 18],
-        [5, 21],
-        [6, 28]
-    ]
-];
-var years = [
-    [0, '2006'],
-    [1, '2007'],
-    [2, '2008'],
-    [3, '2009'],
-    [4, '2010'],
-    [5, '2011'],
-    [6, '2012']
-];
+
+var population = 1000000;
+var testGroup = []; // a variable to hold my fake population of numbers
+var groups = 1000;
+
+// stuffing an array with numbers, just mvp of using the flotr library
+// this algorithm of generation of numbers is simply using Math.random
+for (var i = 0; i < population; i++) {
+    testGroup.push(Math.random());
+}
+
+// give an array of numbers, and the number of bars to represent them, 
+// return array of [ barIndex, qty ] where the barIndex is the 
+// value of the sub population by bracket, and the qty, the number of 
+// members that are in that population.
+function makeRangedChartArrayBar(population, number) {
+    // don't mess with the original array, map it, then sort it to new variable
+    var sortedPop = population.map((elem)=>elem).sort((a, b)=>a - b);
+    var range = sortedPop[sortedPop.length - 1] - sortedPop[0];
+    var total = sortedPop.length;
+    var i;
+    var result = [];
+    var increment = range / number;
+    var currentEnd = increment;
+    var currentSubPop = 0;
+    
+    for (i = 0; i < number; i++) {
+        result.push([i, 0]);
+    }
+    
+    for (i = 0; i < total - 1 && currentSubPop < result.length; i++) {
+        if (sortedPop[i] < currentEnd) {
+            result[currentSubPop][1]++;
+        } else {
+            currentSubPop++;
+            currentEnd += increment;
+            i--; // try that one again next loop, find a home for that element.
+        }
+    }
+    return [result];
+}
 
 window.onload = function drawChart() {
     Flotr.draw(
         document.getElementById('chart'),
-        wins,
+        makeRangedChartArrayBar(testGroup, groups),
         {
-            title: 'Manchester Title Wins',
+            title: 'Distribution of population of ' + testGroup.length + ' numbers into ' + groups + ' groups',
             bars: {
                 show: true,
                 barWidth: 0.5,
@@ -37,7 +58,7 @@ window.onload = function drawChart() {
                 tickDecimals: 0
             },
             xaxis: {
-                ticks: years
+                tickDecimals: 0
             },
             grid: {
                 verticalLines: false
